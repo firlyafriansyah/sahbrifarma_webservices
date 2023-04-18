efabe3f300487112a70ea5e7ce4d6ad17a0c87a4
firlyafriansyah <firlyafriansyah50@hotmail.com>
2023-04-18 11:06:36 +0700

    change headers requirement

--- routes/handler/Administration/GetAdministrationAccount/index.js
+++ routes/handler/Administration/GetAdministrationAccount/index.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
 
   const administrationAccount = await AdministrationAccount.findOne({
     where: { uid },
-    attributes: ['username', 'role', ['updated_at', 'lastUpdated']],
+    attributes: ['username', 'role', ['updated_at', 'updatedAt'], ['last_update', 'lastUpdate']],
   });
 
   if (!administrationAccount) {
