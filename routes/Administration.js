9507a93b5cc8f22d5708c8ef8a506e1b2cfd9cdb
firlyafriansyah <firlyafriansyah50@hotmail.com>
2023-04-21 21:45:43 +0700

    update administration end point

--- routes/Administration.js
+++ routes/Administration.js
@@ -12,8 +12,8 @@ router.post('/logout/:uid', middleware.Logout, administrationHandler.Logout);
 router.get('/', middleware.SuperAuthorization, administrationHandler.GetAdministrationAccountList);
 router.get('/detail/:uid', middleware.SuperAuthorization, administrationHandler.GetAdministrationAccount);
 router.put('/update/:uid', middleware.SuperAuthorization, administrationHandler.UpdateAdministrationAccount);
-router.delete('/delete/:uid', middleware.SuperAuthorization, administrationHandler.DeleteAdministrationAccount);
 router.put('/disabled/:uid', middleware.SuperAuthorization, administrationHandler.DisabledAdministrationAccount);
 router.put('/actived/:uid', middleware.SuperAuthorization, administrationHandler.ActivedAdministrationAccount);
+router.delete('/delete/:uid', middleware.SuperAuthorization, administrationHandler.DeleteAdministrationAccount);
 
 module.exports = router;
