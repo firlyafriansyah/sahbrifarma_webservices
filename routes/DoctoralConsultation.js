d1829b84239d16a5db7952adf0a87a5f3991e209
Firly-Afriansyah <firlyafriansyah50@gmail.com>
2023-05-06 15:28:02 +0700

    finishing setup doctoral consultation handler & routes

--- routes/DoctoralConsultation.js
+++ routes/DoctoralConsultation.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const middleware = require('../middleware');
 const doctoralConsultationHandler = require('./handler/DoctoralConsultation');
 
-router.post('/:uidPatient', middleware.doctoralConsultationHandler, doctoralConsultationHandler.AddDoctoralConsultation);
-router.get('/:uidPatient', middleware.doctoralConsultationHandler, doctoralConsultationHandler.GetDoctoralConsultationList);
-router.get('/detail/:uid', middleware.doctoralConsultationHandler, doctoralConsultationHandler.GetDoctoralConsultationDetail);
+router.post('/:uidPatient', middleware.DoctorAuthorization, doctoralConsultationHandler.AddDoctoralConsultation);
+router.get('/:uidPatient', middleware.DoctorAuthorization, doctoralConsultationHandler.GetDoctoralConsultationList);
+router.get('/detail/:uid', middleware.DoctorAuthorization, doctoralConsultationHandler.GetDoctoralConsultationDetail);
 router.put('/update/:uidPatient', middleware.SuperAuthorization, doctoralConsultationHandler.UpdateDoctoralConsultation);
 router.delete('/delete/:uid', middleware.SuperAuthorization, doctoralConsultationHandler.DeleteDoctoralConsultation);
 
