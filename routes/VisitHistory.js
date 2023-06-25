fdd130a1a821e501129a2d719860063e1d7213b3
Firly Afriansyah <firlyafriansyah50@gmail.com>
2023-06-25 19:40:36 +0700

    add feature visit history

--- routes/VisitHistory.js
+++ routes/VisitHistory.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const middleware = require('../middleware');
 const visitHistoryHandler = require('./handler/VisitHistory');
 
-router.get('/', middleware.FrontdeskAuthorization, visitHistoryHandler.VisitHistory);
+router.get('/:uidPatient', middleware.FrontdeskAuthorization, visitHistoryHandler.VisitHistory);
 
 module.exports = router;
