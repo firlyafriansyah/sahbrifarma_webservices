fe2b472edf15b7c9ca0f0f0674bc66f32c3772a9
Firly Afriansyah <firlyafriansyah50@gmail.com>
2023-06-25 09:02:42 +0700

    fixed bug registration administration

--- EncryptText.js
+++ EncryptText.js
@@ -1,10 +1,10 @@
 const CryptoJS = require('crypto-js');
 
 // Authorization
-// const text = CryptoJS.AES.encrypt('5', '_f1rly_').toString();
+const text = CryptoJS.AES.encrypt('1', '_f1rly_').toString();
 
 // Authentication
-const text = CryptoJS.AES.encrypt('5?dinda123', '_f1rly_').toString();
+// const text = CryptoJS.AES.encrypt('5?dinda123', '_f1rly_').toString();
 
 // eslint-disable-next-line no-console
 console.log(text);
