9e2832b1e0f7ee93ebd5bee283c56495a7e8ba24
Firly Afriansyah <firlyafriansyah50@gmail.com>
2023-07-01 08:50:14 +0700

    change authorization for get medical test detail

--- routes/MedicalTest.js
+++ routes/MedicalTest.js
@@ -6,7 +6,7 @@ const middleware = require('../middleware');
 const medicalTestHandler = require('./handler/MedicalTest');
 
 router.post('/:uidPatient', middleware.NurseAuthorization, medicalTestHandler.AddMedicalTest);
-router.get('/detail/:uid', middleware.NurseAuthorization, medicalTestHandler.GetMedicalTestDetail);
+router.get('/detail/:uid', medicalTestHandler.GetMedicalTestDetail);
 router.get('/:uidPatient', medicalTestHandler.GetMedicalTestList);
 router.put('/update/:uid', middleware.SuperAuthorization, medicalTestHandler.UpdateMedicalTest);
 
